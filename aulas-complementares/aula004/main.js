152cd742a06fac73adc2f7712c4d3b675f116715
Vinicius Duarte <viniciusduarte@outlook.com>
2021-10-12 22:32:07 -0300

    pre aula 4, 27:32

--- aulas-complementares/aula004/main.js
+++ aulas-complementares/aula004/main.js
@@ -1,8 +1,48 @@
 const person = {
-  name: 'Vinicius', 
+  name: 'Vinicius',
+  surname: 'Duarte',
   age: 25
 }
 
-const personUpdated = person 
+const personUpdated = {
+  ...person,
+  age: 26
+}
 
-console.log(personUpdated)
+const personWithBirthYear = {
+  ...person,
+  birthyear: 1995
+}
+
+console.log('person: ', person)
+console.log('personUpdated: ', personUpdated)
+console.log('personWithBirthYear', personWithBirthYear)
+
+const arr = [10, 20, 30]
+//const arr2 = arr.concat(40)
+const arr2 = [
+  ...arr,
+  40
+]
+
+console.log('arr: ', arr)
+console.log('arr2: ', arr2)
+
+const str = 'Vinicius'
+//const arrStr = str.split('')
+const arrStr = [...str]
+
+console.log('str: ', str)
+console.log('arrStr: ', arrStr)
+
+// const newPerson = {
+//   ...personWithBirthYear,
+// }
+
+// delete newPerson.age
+
+const newPerson = {
+  ...personWithBirthYear,
+}
+
+console.log('newPerson', newPerson)
