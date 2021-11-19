c8b37de71690a1c8d12f5abbe8609ec6764fb403
Vinicius Duarte <viniciusduarte@outlook.com>
2021-11-18 22:56:52 -0300

    exerc 2 incomplete.

--- bootcamp/aula007-desafio01/src/app.js
+++ bootcamp/aula007-desafio01/src/app.js
@@ -1,6 +1,48 @@
 function App () {
+
+  function Header () {
+    return (
+      <header>HEADER</header>
+    )
+  }
+
+  function Menu () {
+    return (
+      <ul>
+        <li>Menu</li>
+        <li>Menu</li>
+        <li>Menu</li>
+      </ul>
+    )
+  }
+
+  function Sidebar () {
+    return (
+      <aside>Sidebar</aside>
+    )
+  }
   
-  return <h1>Teste</h1>
+  function Content () {
+    return (
+      <div>Content</div>
+    )
+  }
+
+  function Footer () {
+    return (
+      <footer>Sidebar</footer>
+    )
+  }
+
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Sidebar />
+      <Content />
+      <Footer />
+    </>
+  )
 }
 
 export default App
