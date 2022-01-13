5fbac21914fa4418d606cba5204ecb8a237a35bb
Vinicius Duarte <viniciusduarte@outlook.com>
2022-01-13 16:54:06 -0300

    Finish aula017

---
+++ bootcamp/aula017/src/App.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+function App () {
+  const [counter, setCounter] = useState(1)
+
+  return (
+    <>
+      {counter > 0 && <Counter counter={counter} />}
+      <button onClick={() => setCounter((c) => c-1)}>-</button>
+      <button onClick={() => setCounter((c) => c+1)}>+</button>
+    </>
+  )
+}
+
+function Counter( { counter } ) {
+  console.log('counter')
+
+  useEffect( () => {
+    console.log('useEffect de Counter')
+    
+    return () => {
+      console.log('clean effect de counter')
+    }
+  })
+
+  return (
+    <h1>{counter}</h1>
+  )
+}
+
+export default App;
